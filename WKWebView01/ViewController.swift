9ad460b88599e64b59648ec6fc9a8d6348929597
Jonghyun Kim <jhkim@dit.ac.kr>
2019-09-05 14:36:53 +0900

    1st common

--- WKWebView01/ViewController.swift
+++ WKWebView01/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        
+        let request = URLRequest(url: URL(string: "http://www.naver.com")!)
+        
+        webView.load(request)
     }
 
-
+    @IBAction func backAction(_ sender: Any) {
+        if self.webView.canGoBack {
+            self.webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardAction(_ sender: Any) {
+        if self.webView.canGoForward {
+            self.webView.goForward()
+        }
+    }
+    
+    // UISearchBarDelegate 메소드 호출
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let inputURL = searchBar.text!
+        let request = URLRequest(url: URL(string: inputURL)!)
+        webView.load(request)
+        //다른곳을 터치하면 키보드가 내려갈 수 있도록 한다.
+        self.view.endEditing(true)
+    }
 }
 
